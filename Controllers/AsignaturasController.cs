91f68535fe27f3358c70a90cfc0836f80e795191
Nicolas Fiquitiva Segura <Nico52105@gmail.com>
2021-07-18 18:05:42 -0500

    Clase 13
    Repaso del modelo MVC y creacion de un nuevo controlador y vista para el modelo de asignatura

--- Controllers/AsignaturasController.cs
+++ Controllers/AsignaturasController.cs
@@ -1,7 +1,20 @@
+using System;
+using EjercicioMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace EjercicioMVC.Controllers
 {
-    public class AsignaturasController
+    public class AsignaturaController: Controller
     {
-        
+        public IActionResult Index()
+        {
+            var asignatura = new Asignatura();
+            asignatura.Nombre="Programacion";
+            asignatura.UniqueId=Guid.NewGuid().ToString();
+            
+            ViewBag.informacionAdicional="+ informacion de Asignatura";
+            
+            return View(asignatura);
+        }
     }
 }
