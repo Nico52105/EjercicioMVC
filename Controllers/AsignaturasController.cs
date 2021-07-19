4ceb34555349bc5728c5f98ad6a501ff0f96d659
Nicolas Fiquitiva Segura <Nico52105@gmail.com>
2021-07-18 19:53:48 -0500

    Clase 14
    Se creo una vista parcial para mostrar los datos del  modelo Asignatura
    y se realizo una implementacion en la Vista Index de Asignatura, Luego se creo un metodo en el controlador para enviar multiples asignaturas a la vista la cual mediante un foreach y la vista parcial permitiera la visualizacion de multiples Asignaturas.

--- Controllers/AsignaturasController.cs
+++ Controllers/AsignaturasController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using EjercicioMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,6 +16,22 @@ namespace EjercicioMVC.Controllers
             ViewBag.informacionAdicional="+ informacion de Asignatura";
             
             return View(asignatura);
+        }
+
+        public IActionResult Todas()
+        {
+            List<Asignatura> asignaturas= new List<Asignatura>();
+            for (int i = 0; i < 5; i++)
+            {
+                var asignatura = new Asignatura();
+                asignatura.Nombre="Asignatura "+i;
+                asignatura.UniqueId=Guid.NewGuid().ToString();
+                asignaturas.Add(asignatura);
+            }
+                        
+            ViewBag.informacionAdicional="+ informacion de todas las Asignaturas";
+            
+            return View(asignaturas);
         }
     }
 }
